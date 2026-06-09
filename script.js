27620940802beca6a4d98058e44b8b669252205b
chamssadraka-svg <chamssadraka@gmail.com>
2026-06-09 08:32:07 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+function verifierOuverture() {
+    const maintenant = new Date();
+    
+    const jour = maintenant.getDay(); 
+
+    const heure = maintenant.getHours();
+    const minutes = maintenant.getMinutes();
+    const heureActuelleEnMinutes = (heure * 60) + minutes;
+
+    const heureOuverture1 = (8 * 60) + 0;  
+    const heureFermeture1 = (12 * 60) + 0;
+    const heureOuverture2 = (15 * 60) + 0;
+    const heureFermeture2 = (18 * 60) + 0; 
+
+    const baliseStatus = document.getElementById("status-magasin");
+
+    if (jour === 0 || heureActuelleEnMinutes < heureOuverture1 || heureOuverture2 > heureActuelleEnMinutes >= heureFermeture1 || heureActuelleEnMinutes >= heureFermeture2) {
+        baliseStatus.textContent = "fermé";
+        baliseStatus.style.color = "red";
+    } else {
+        baliseStatus.textContent = "ouvert";
+        baliseStatus.style.color = "green";
+    }
+}
+
+verifierOuverture();
+
+setInterval(verifierOuverture, 1000);
